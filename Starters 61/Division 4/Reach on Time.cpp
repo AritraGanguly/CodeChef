893c6e5f375200d6a03bb386a18249475a7b16b1
Aritra Ganguly <84271287+AritraGanguly@users.noreply.github.com>
2022-10-20 21:37:53 +0530

    Create Reach on Time.cpp

---
+++ Starters 61/Division 4/Reach on Time.cpp
@@ -0,0 +1,18 @@
+include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes 
+	int t;
+	cin>>t;
+	while(t!=0){
+	    int x;
+	    cin>>x;
+	    if(x>=30)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	    t--;
+	}
+	return 0;
+}
